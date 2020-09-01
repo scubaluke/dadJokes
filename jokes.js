a923b80b55ce96865bb64df653665e8f20ea14df
Luke Gartland <63882666+scubaluke@users.noreply.github.com>
2020-08-31 16:13:52 -1000

    dad joke web app

---
+++ jokes.js
@@ -0,0 +1,53 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+const buttonText = [
+  'Ugh.',
+  '🤦🏻‍♂️',
+  'omg dad.',
+  'you are the worst',
+  'seriously',
+  'stop it.',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  // turn  loader on
+  // loader.classList.remove('hidden');
+  jokeHolder.innerHTML = ` <div class="lds-ellipsis loader ">
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>`;
+
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = response.json();
+  // turn loader off
+  // loader.classList.add('hidden');
+  return data;
+}
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log('we used that one last time');
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
+}
+jokeButton.addEventListener('click', handleClick);
